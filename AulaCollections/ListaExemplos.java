862e864badda516ee45f1b31f1741d1c99143684
Ilton Baracho <iltonbaracho@gmail.com>
2020-06-02 11:15:20 -0300

    Inserindo aula collections

---
+++ AulaCollections/ListaExemplos.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ListaExemplos {
+
+	public static void main(String[] args) {
+		
+		List<String> nomes = new ArrayList<>();
+
+		nomes.add("Ilton");
+		nomes.add("Marcos");
+		nomes.add("João");
+		nomes.add("Maria");
+		nomes.add("Luciano");
+		nomes.add("Altair");
+
+
+		System.out.println("\n Nomes conforme inseridos: \n" + nomes + "\n\n");
+	}
+
+
+}
