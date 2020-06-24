9804a4edb20cfc16e32fbcdb8c04a24626c1f51c
Ilton Baracho <iltonbaracho@gmail.com>
2020-06-24 08:06:28 -0300

    Inserindo a pasta aula_javascript

---
+++ aula_javascript/js/main.js
@@ -0,0 +1,7 @@
+
+var nome = "Ilton Baracho";
+var idade = 42;
+var mais1Ano = idade + 1;
+alert ("Bem vindo " + nome + ", você tem " + idade + " anos !");
+alert ("Idade daqui a um ano = " + mais1Ano);
+
