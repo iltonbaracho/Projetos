974a545514edd8accdc605d71cc5bbb7f2b62739
Ilton Baracho <iltonbaracho@gmail.com>
2020-06-25 11:03:55 -0300

    inclusão instruções no main.js

--- aula_javascript/js/main.js
+++ aula_javascript/js/main.js
@@ -5,3 +5,23 @@ var mais1Ano = idade + 1;
 alert ("Bem vindo " + nome + ", você tem " + idade + " anos !");
 alert ("Idade daqui a um ano = " + mais1Ano);
 
+produtos = [
+{
+	nome: "Ilton",
+	idade: 42,
+	cargo: "Analista"
+},
+{
+	nome: "Perão",
+	idade: 43,
+	cargo: "Gerente"	
+},
+{
+	nome: "Kayo",
+	idade: 28,
+	cargo: "Assistente"
+}
+];
+console.log(produtos);
+console.log(produtos[2].nome);
+console.log(produtos[2].cargo);
